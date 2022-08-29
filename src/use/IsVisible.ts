54a12d91ce0b5e3b6e016bd5ef4db9bcdfc57e45
huytv <tranvanhuy0202@gmail.com>
2022-08-30 00:23:18 +0700

    Update isVisible

--- src/use/IsVisible.ts
+++ src/use/IsVisible.ts
@@ -2,18 +2,118 @@ const isVisible = (
   data: Record<string, any>,
   option: Record<string, any>,
 ): boolean => {
+  // #1. No dependency
   if (!option.dependency) {
     return true;
   }
 
-  const dependencyName = option.dependency.name;
-  const dependencyValue = option.dependency.value;
 
-  if (data[dependencyName] !== dependencyValue) {
-    return false;
+  // #2. Has dependency
+
+  // There're 2 types of value
+  //  - primary (1): String/Number/Boolean
+  //  - object (n): Array/Object
+  // There're 3 supported relationships
+  //
+  // |--------|---------|-------------------------
+  // | Master | Servant | Checking value(s)
+  // |--------|---------|-------------------------
+  // |   1    |    1    | value/value_not_equal
+  // |   1    |    n    | value_in/value_not_in
+  // |   n    |    1    | value_has/value_not_has
+
+  const {
+    name,
+    value,
+    value_not_equal: valueNotEqual,
+    value_in: valueIn,
+    value_not_in: valueNotIn,
+    value_has: valueHas,
+    value_not_has: valueNotHas,
+  } = option.dependency;
+
+  console.log({
+    name,
+    value,
+    value_not_equal: valueNotEqual,
+    value_in: valueIn,
+    value_not_in: valueNotIn,
+    value_has: valueHas,
+    value_not_has: valueNotHas,
+  });
+
+  if (!name) {
+    throw new Error(`Option ${option.name}: dependency element is undefined`);
   }
 
-  return true;
+  const dependencyValue = data[name];
+
+  // 1. Value equal
+  if (value !== undefined) {
+    // this will not work in case of using dependency as below
+    // <?php
+    //    // no 'default_value'
+    //    'dependency' => [
+    //      'element' => 'dependency_element',
+    //      'value' => false,
+    //    ],
+    // ?>
+    // in that case: this.data[element] === undefined !== false
+    // ***Solution: if an option depends on a Boolean option, only use 'value' => true or
+    //              change logic to use 'value_not_equal'
+    if (dependencyValue === value) {
+      return true;
+    }
+  }
+
+  // 2. Value not equal
+  if (valueNotEqual !== undefined) {
+    if (dependencyValue !== valueNotEqual) {
+      return true;
+    }
+  }
+
+  // 3. Value in
+  if (valueIn !== undefined) {
+    const values = valueIn.split(',').map(v => v.trim());
+
+    if (values.includes(dependencyValue)) {
+      return true;
+    }
+  }
+
+  // 4. Value not in
+  if (valueNotIn !== undefined) {
+    const values = valueNotIn.split(',').map(v => v.trim());
+
+    if (!values.includes(dependencyValue)) {
+      return true;
+    }
+  }
+
+  // 5. Value has
+  if (valueHas !== undefined) {
+    if (!dependencyValue.isArray()) {
+      throw new Error(`Option ${option.name}: dependency ${name} value must be an array`);
+    }
+
+    if (dependencyValue.includes(valueHas)) {
+      return true;
+    }
+  }
+
+  // 6. Value not has
+  if (valueNotHas !== undefined) {
+    if (!dependencyValue.isArray()) {
+      throw new Error(`Option ${option.name}: dependency ${name} value must be an array`);
+    }
+
+    if (!dependencyValue.includes(valueNotHas)) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 export default isVisible;
